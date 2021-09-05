ec3c16c7ee7deb6cc9c6c56d062d23022d275de9
Hariprasath R <balaprasad24@gmail.com>
2021-09-05 12:23:01 +0530

    Joining Multiple Tables

--- SQL-Course-Materials/sql-join-scratchpad.sql
+++ SQL-Course-Materials/sql-join-scratchpad.sql
@@ -1,4 +1,6 @@
---- INNER JOIN example
+USE sql_store;
+
+-- INNER JOIN example
 SELECT order_id, p.product_id, p.name, o.quantity, o.unit_price
 FROM order_items o
 INNER JOIN products p
@@ -20,3 +22,31 @@ SELECT
 FROM sql_hr.employees e
 JOIN sql_hr.employees m
 	ON e.employee_id = m.employee_id;
+	
+-- Joining multiple tables
+SELECT
+	o.order_id,
+	c.first_name,
+	c.last_name,
+	o.order_date,
+	os.name AS order_status
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id
+JOIN order_statuses os
+	ON o.`status` = os.order_status_id;
+	
+USE sql_invoicing;
+SELECT
+	p.date AS payment_date,
+	i.invoice_id,
+	c.name AS client_name,
+	i.invoice_total,
+	pm.name AS payment_method
+FROM payments p
+JOIN clients c
+	ON p.client_id = c.client_id
+JOIN invoices i
+	ON p.invoice_id = i.invoice_id
+JOIN payment_methods pm
+	ON p.payment_method = pm.payment_method_id
